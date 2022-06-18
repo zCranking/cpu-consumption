faf0637afb0dc0464738010bab602803b43de968
zCranking <aravgupta2019@gmail.com>
2022-06-17 19:12:42 -0700

    init

---
+++ cpustuff.py
@@ -0,0 +1,32 @@
+import matplotlib .pyplot as plt
+import psutil as p
+
+names = {}
+count = 0
+
+for process in p.process_iter():
+    count = count + 1
+    if count <= 6:
+        name = process.name()
+        usage = p.cpu_percent()
+        print("Name: " + str(name) + " Usage: " + str(usage))
+        names.update({name:usage})
+        
+keymax = max(names, key=names.get)
+print(keymax)
+keymin = min(names, key=names.get)
+print(keymin)
+name_list = [keymax, keymin]
+print(name_list)
+
+app = names.values()
+maxapp = max(app)
+minapp = min(app)
+maxMinlist = [maxapp, minapp]
+print(maxMinlist)
+
+plt.figure(figsize=(15,7))
+plt.xlabel("Apps")
+plt.ylabel("Usage")
+plt.bar(name_list, maxMinlist, width=0.6, color=("blue", "red"))
+plt.show()
